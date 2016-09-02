b88a91840699c040161e2c023eff80305be26536
jvican <jorgevc@fastmail.es>
2016-09-02 15:07:04 +0200

    Add tests for all the serializable primitive types

--- spores-spark/src/test/scala/scala/spores/spark/SerializationSpec.scala
+++ spores-spark/src/test/scala/scala/spores/spark/SerializationSpec.scala
@@ -26,4 +26,58 @@ class SerializationSpec {
       () => println(s"$captured")
     }
   }
+
+  @Test
+  def `A captured Byte is serializable`(): Unit = {
+    val number: Byte = 1
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
+
+  @Test
+  def `A captured Char is serializable`(): Unit = {
+    val number: Char = 1
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
+
+  @Test
+  def `A captured Short is serializable`(): Unit = {
+    val number: Short = 1
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
+
+  @Test
+  def `A captured Long is serializable`(): Unit = {
+    val number: Long = 1
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
+
+  @Test
+  def `A captured Float is serializable`(): Unit = {
+    val number: Float = 1
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
+
+  @Test
+  def `A captured Double is serializable`(): Unit = {
+    val number: Double = 1.0
+    spore {
+      val captured = number
+      () => println(s"$captured")
+    }
+  }
 }
