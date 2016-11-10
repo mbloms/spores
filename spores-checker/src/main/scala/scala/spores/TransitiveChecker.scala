faae3a486b94ecef567dd3b2419944e8cee3d9e7
jvican <jorgevc@fastmail.es>
2016-11-10 14:43:29 +0100

    Extend tests and explain accessor situation

--- spores-checker/src/main/scala/scala/spores/TransitiveChecker.scala
+++ spores-checker/src/main/scala/scala/spores/TransitiveChecker.scala
@@ -11,16 +11,36 @@ class TransitiveChecker[G <: scala.tools.nsc.Global](val global: G) {
         _.tpe.typeSymbol == definitions.TransientAttr)
     }
 
-    override def traverse(tree: Tree): Unit = tree match {
-      case cls: ClassDef =>
-        val members = cls.symbol.info.members
-        val serializableFields = members
+    /** Transitively check that the types of the fields are Serializable.
+      *
+      * Watch out: traverse works by checking the fields of a given symbol.
+      * However, traits only have accessors, not actual fields, and
+      * accessors are ignored in the analysis. This could lead us to false
+      * positives because top-level traits can be instantiated and passed
+      * through the program. This is dangerous and must be checked carefully.
+      */
+    override def traverse(tree: Tree): Unit = {
+      def checkMembers(symbol: Symbol) = {
+        val members = symbol.info.members
+        reporter.info(symbol.pos, s"Found members $members", force = true)
+        val fields = members
           .filter(m => m.isTerm && !m.isMethod && !m.isModule)
           .filterNot(isTransient)
           .toList
-        val msg = s"Found in ${cls.name.decodedName}: $serializableFields"
-        reporter.info(cls.pos, msg, force = true)
-      case _ => super.traverse(tree)
+        val msg = s"Fields in ${symbol.name.decodedName}: $fields"
+        reporter.info(symbol.pos, msg, force = true)
+        fields.foreach { field =>
+          if (!field.isSerializable && field.isNumericValueClass) {
+            reporter.error(field.pos,
+                           s"Not serializable: $field with type ${field.tpe}")
+          }
+        }
+      }
+
+      tree match {
+        case cls: ClassDef => checkMembers(cls.symbol)
+        case _ => super.traverse(tree)
+      }
     }
   }
 }
--- spores-checker/src/test/scala/members/MemberHolder.scala
+++ spores-checker/src/test/scala/members/MemberHolder.scala
@@ -3,8 +3,9 @@ package members
 trait BaseBaseMemberHolder {
   val member5: String
   val member6: Object
+  var member7: List[String]
+  var memberIgnored: Int => Int
 }
-
 
 trait BaseMemberHolder extends BaseBaseMemberHolder {
   val member3: Double
@@ -14,12 +15,17 @@ trait BaseMemberHolder extends BaseBaseMemberHolder {
 abstract class MemberHolder(member0: Int) extends BaseMemberHolder {
   private val member1 = 1
   val member2: String
+  var member8: Int
   println(member1)
 }
 
-class Bar(member0: Int, @transient val member5: String, member10: Int) extends MemberHolder(member0) {
+class Bar(member0: Int, val member5: String, var member8: Int)
+    extends MemberHolder(member0) {
   val member2 = ""
   val member3 = 4.0
-  val member4 = new Integer(1)
-  val member6 = member4
+  val member4: Object = new Integer(1)
+  val member6: Object = member4
+  var member7: List[String] = Nil
+  member7 = List("")
+  @transient var memberIgnored: Int => Int = (a: Int) => a
 }
