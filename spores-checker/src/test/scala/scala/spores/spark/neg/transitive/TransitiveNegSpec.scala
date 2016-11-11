2b940ffdfe0a93ad065eecc6cf39af93c358518f
jvican <jorgevc@fastmail.es>
2016-11-11 15:33:05 +0100

    Add transitive checks for depth 1 and 2

--- spores-checker/src/test/scala/scala/spores/spark/neg/transitive/TransitiveNegSpec.scala
+++ spores-checker/src/test/scala/scala/spores/spark/neg/transitive/TransitiveNegSpec.scala
@@ -1,5 +1,48 @@
 package scala.spores.spark.neg.transitive
 
-class TransitiveNegSpec {
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+import scala.spores.spark.TestUtil._
+import scala.spores.util.Feedback._
+
+@RunWith(classOf[JUnit4])
+class TransitiveNegSpec {
+  @Test
+  def `Depth 1: Non serializable fields in classes are detected`(): Unit = {
+    expectError(
+      NonSerializableType("Foo", "value bar", "Bar")
+    ) {
+      """
+        |import scala.spores._
+        |class Bar
+        |class Foo(val bar: Bar) extends Serializable
+        |val foo = new Foo(new Bar)
+        |spore {
+        |  val captured = foo
+        |  () => captured
+        |}
+      """.stripMargin
+    }
+  }
+
+  @Test
+  def `Depth 2: Non serializable fields in classes are detected`(): Unit = {
+    expectError(
+      NonSerializableType("Bar", "value baz", "Baz")
+    ) {
+      """
+        |import scala.spores._
+        |abstract class Baz
+        |class Bar(val baz: Baz) extends Serializable
+        |class Foo(val bar: Bar) extends Serializable
+        |val foo = new Foo(new Bar(new Baz {}))
+        |spore {
+        |  val captured = foo
+        |  () => captured
+        |}
+      """.stripMargin
+    }
+  }
 }
