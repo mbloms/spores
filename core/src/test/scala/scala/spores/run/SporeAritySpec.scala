f053301d0866dd39affeb1dd2c10b8a3cf0ea6fa
jvican <jorgevc@fastmail.es>
2016-12-01 15:44:53 +0100

    Add tests for all the spore arities

--- core/src/test/scala/scala/spores/run/SporeAritySpec.scala
+++ core/src/test/scala/scala/spores/run/SporeAritySpec.scala
@@ -83,4 +83,130 @@ class SporeAritySpec {
     }
     assert(s(1, 1, 1, 1, 1, 1, 1, 1) == 50)
   }
+
+  @Test
+  def spore9(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1) == 51)
+  }
+
+  @Test
+  def spore10(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 52)
+  }
+
+  @Test
+  def spore11(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 53)
+  }
+
+  @Test
+  def spore12(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 54)
+  }
+
+  @Test
+  def spore13(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 55)
+  }
+
+  @Test
+  def spore14(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 56)
+  }
+
+  @Test
+  def spore15(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 57)
+  }
+
+  @Test
+  def spore16(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 58)
+  }
+
+  @Test
+  def spore17(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 59)
+  }
+
+  @Test
+  def spore18(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int, i18: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 60)
+  }
+
+  @Test
+  def spore19(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int, i18: Int, i19: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 61)
+  }
+
+  @Test
+  def spore20(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int, i18: Int, i19: Int, i20: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19 + i20
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 62)
+  }
+
+  @Test
+  def spore21(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int, i18: Int, i19: Int, i20: Int, i21: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19 + i20 + i21
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 63)
+  }
+
+  @Test
+  def spore22(): Unit = {
+    val s = spore {
+      (i: Int, i2: Int, i3: Int, i4: Int, i5: Int, i6: Int, i7: Int, i8: Int, i9: Int, i10: Int, i11: Int, i12: Int, i13: Int, i14: Int, i15: Int, i16: Int, i17: Int, i18: Int, i19: Int, i20: Int, i21: Int, i22: Int) =>
+        42 + i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 + i12 + i13 + i14 + i15 + i16 + i17 + i18 + i19 + i20 + i21 + i22
+    }
+    assert(s(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 64)
+  }
 }
